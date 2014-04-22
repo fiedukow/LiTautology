273128b2e3d92e66347fe2a6461a8add1ae440ff
fiedukow <fiedukow@gmail.com>
2014-04-22 21:13:36 +0200

    Adjusting Minisat code to fit n-hetmans needs.

---
+++ minisat/simp/Main.cc
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <zlib.h>
+#include <cstdio>
+
+#include "utils/System.h"
+#include "utils/ParseUtils.h"
+#include "utils/Options.h"
+#include "core/Dimacs.h"
+#include "simp/SimpSolver.h"
+
+using namespace Minisat;
+
+// xxx ooo ooo
+// ooo xxx ooo
+// ooo ooo xxx
+
+std::string genRows(const int N) {
+  std::stringstream ss;
+  for (int i = 0; i < N; ++i) {
+    // every line
+    // generate a1 or a2 or ... or an
+    for (int j = 1; j <= N; ++j) {
+      ss << j+(N*i) << " ";
+    }
+    ss << std::endl;
+    // generate not aj or not ak, where j != k
+    for (int j = 1; j <= N; ++j) {
+      for (int k = j+1; k <= N; ++k) {
+        ss << -(j+(N*i)) << " " << -(k+(N*i)) << std::endl;
+      }
+    }
+  }
+  return ss.str();
+}
+
+// xoo oxo oox
+// xoo oxo oox
+// xoo oxo oox
+std::string genCols(const int N) {
+  std::stringstream ss;
+  for (int i = 1; i <= N; ++i) {
+    // every column
+    // generate a1 or a(1+N) or ... or a(1+N(N-1))
+    for (int j = 0; j < N; ++j) {
+      ss << i+(N*j) << " ";
+    }
+    ss << std::endl;
+    // generate not aj or not ak, where j != k
+    for (int j = 0; j < N; ++j) {
+      for (int k = j+1; k < N; ++k) {
+        ss << -(i + N*j) << " " << -(i + N*k) << std::endl;
+      }
+    }
+  }
+  return ss.str();
+}
+
+// xoo oxo oox
+// oxo oox ooo
+// oox ooo ooo
+//
+// ooo ooo
+// xoo ooo
+// oxo xoo
+//
+// oox oxo xoo
+// oxo xoo ooo
+// xoo ooo ooo
+//
+// ooo ooo
+// oox ooo
+// oxo oox
+std::string genDiags(const int N) {
+  std::stringstream ss;
+  for (int i = 1; i <= N*N; ++i) {
+    for (int j = i+N+1; j <= N*N && i%N != 0; j += N+1) {
+      if((j-(N+1))%N == 0)
+        break;
+      ss << -i << " " << -j << std::endl;
+    }
+  }
+  
+  for (int i = 1; i <= N*N; ++i) {
+    for (int j = i+N-1; j <= N*N; j += N-1) {
+      if(j%N == 0)
+        break;
+      ss << -i << " " << -j << std::endl;
+    }
+  }
+  return ss.str();
+}
+
+std::string genAll(const int N) {
+  std::stringstream ss;
+  ss << genRows(N);
+  ss << genCols(N);
+  ss << genDiags(N);
+  return ss.str();
+}
+
+void printStats(Solver& solver)
+{
+    double cpu_time = cpuTime();
+    double mem_used = memUsedPeak();
+    printf("restarts              : %"PRIu64"\n", solver.starts);
+    printf("conflicts             : %-12"PRIu64"   (%.0f /sec)\n", solver.conflicts   , solver.conflicts   /cpu_time);
+    printf("decisions             : %-12"PRIu64"   (%4.2f %% random) (%.0f /sec)\n", solver.decisions, (float)solver.rnd_decisions*100 / (float)solver.decisions, solver.decisions   /cpu_time);
+    printf("propagations          : %-12"PRIu64"   (%.0f /sec)\n", solver.propagations, solver.propagations/cpu_time);
+    printf("conflict literals     : %-12"PRIu64"   (%4.2f %% deleted)\n", solver.tot_literals, (solver.max_literals - solver.tot_literals)*100 / (double)solver.max_literals);
+    if (mem_used != 0) printf("Memory used           : %.2f MB\n", mem_used);
+    printf("CPU time              : %g s\n", cpu_time);
+}
+
+
+int main(int argc, char* argv[]) {
+  assert(argc > 1);
+  const int N = atoi(argv[1]);
+  std::ofstream fout;
+  fout.open("formula", std::ofstream::out);
+  fout << genAll(N) << std::endl;
+  fout.close();
+  gzFile in = gzopen("formula", "rb");
+
+  SimpSolver  S;
+  parse_DIMACS(in, S);
+  gzclose(in);
+  S.eliminate(true);
+  vec<Lit> dummy;
+  lbool ret = S.solveLimited(dummy);
+  printStats(S);
+  printf(ret == l_True ? "SATISFIABLE\n" : ret == l_False ? "UNSATISFIABLE\n" : "INDETERMINATE\n");
+  FILE* res = stdout;
+  if (res != NULL){
+    if (ret == l_True){
+        fprintf(res, "SAT\n");
+        for (int i = 0; i < S.nVars(); i++)
+            if (S.model[i] != l_Undef)
+                fprintf(res, "%s%s%d", (i==0)?"":" ", (S.model[i]==l_True)?"":"-", i+1);
+        fprintf(res, " 0\n");
+    }else if (ret == l_False)
+        fprintf(res, "UNSAT\n");
+    else
+        fprintf(res, "INDET\n");
+    fclose(res);
+  }   
+}
