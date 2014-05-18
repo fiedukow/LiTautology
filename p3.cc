e97ed33ce51303c54a5f2a3972dec0c77c7c067d
fiedukow <fiedukow@gmail.com>
2014-05-18 16:36:19 +0200

    Real reduct created.

--- p3.cc
+++ p3.cc
@@ -10,6 +10,10 @@
 
 typedef std::vector<int> Diff;
 
+struct Reduct {
+  std::vector<int> mul;
+};
+
 bool operator<(const Diff& d1, const Diff& d2) {
   for (int i : d1) {
     bool has = false;
@@ -73,7 +77,7 @@ public:
 class DiffTable {
 public:
   DiffTable(const Table& t, bool info_system = true);
-  std::vector<Diff> reducts() const;
+  std::vector<Reduct> reducts() const;
 
   friend std::ostream& operator<<(std::ostream& os, const DiffTable& t);
 
@@ -81,27 +85,6 @@ private:
   const Table& t;
   std::vector<DiffWithId> diffs;
 };
-
-DiffTable::DiffTable(const Table& t, bool info_system)
-    : t(t) {
-  for (int i = 0; i < t.lines.size(); ++i)
-    for (int j = i + 1; j < t.lines.size(); ++j)
-      if (info_system || t.lines[i].decision != t.lines[j].decision)
-        diffs.push_back(DiffWithId(t.lines[i].diff(t.lines[j]), i, j));
-}
-
-std::vector<Diff> DiffTable::reducts() const {
-  std::vector<Diff> reducts;
-  for (const DiffWithId& di : diffs)
-    InsertWithReduction(reducts, di.diff);
-  return reducts;
-}
-
-std::ostream& operator<<(std::ostream& os, const DiffTable& t) {
-  for(DiffWithId diff : t.diffs)
-    os << diff << std::endl;
-  return os;
-}
 
 std::ostream& operator<<(std::ostream& os, const DiffWithId& diff) {
   os << diff.id1 << " x " << diff.id2 << ": ";
@@ -135,6 +118,73 @@ std::ostream& operator<<(std::ostream& os, const std::vector<Diff> diffs) {
   return os;
 }
 
+std::ostream& operator<<(std::ostream& os, const Reduct& rd) {
+  for (int i = 0; i < rd.mul.size(); ++i) {
+    os << rd.mul[i] << (i == rd.mul.size()-1 ? "" : "*");
+  }
+  return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<Reduct>& rds) {
+  for (int i = 0; i < rds.size(); ++i) {
+    os << rds[i] << (i == rds.size()-1 ? "" : "+");
+  }
+  return os;
+}
+
+DiffTable::DiffTable(const Table& t, bool info_system)
+    : t(t) {
+  for (int i = 0; i < t.lines.size(); ++i)
+    for (int j = i + 1; j < t.lines.size(); ++j)
+      if (info_system || t.lines[i].decision != t.lines[j].decision)
+        diffs.push_back(DiffWithId(t.lines[i].diff(t.lines[j]), i, j));
+}
+
+std::vector<Reduct> operator*(const Reduct& r, const Diff sum) {
+  std::vector<Reduct> rs;
+  for (int d : sum) {
+    Reduct rn = r;
+    rn.mul.push_back(d);
+    rs.push_back(rn);
+  }
+  return rs;
+}
+
+std::vector<Reduct> operator*(const std::vector<Reduct>& r, const Diff& diff) {
+  std::vector<Reduct> result;
+  if (r.empty()) {
+    result = Reduct()*diff;
+    return result;
+  }
+  for (const Reduct& ri : r) {
+    std::vector<Reduct> res = ri * diff;
+    result.insert(result.end(), res.begin(), res.end());
+  }
+  return result;
+}
+
+std::vector<Reduct> ToReducts(const std::vector<Diff>& sums) {
+  std::vector<Reduct> result;
+  for (Diff diff : sums) {
+    result = result * diff;
+  }
+  return result;
+}
+
+std::vector<Reduct> DiffTable::reducts() const {
+  std::vector<Diff> sums;
+  for (const DiffWithId& di : diffs)
+    InsertWithReduction(sums, di.diff);
+  std::cout << sums << std::endl;
+  return ToReducts(sums);
+}
+
+std::ostream& operator<<(std::ostream& os, const DiffTable& t) {
+  for(DiffWithId diff : t.diffs)
+    os << diff << std::endl;
+  return os;
+}
+
 Table::Table(const std::string& filePath) {
   std::ifstream fs_in(filePath.c_str());
   std::string line;
@@ -164,6 +214,6 @@ int main() {
   std::cout << t << std::endl;
   DiffTable dt(t, false);
   std::cout << dt << std::endl;
-  std::vector<Diff> diffs = dt.reducts();
-  std::cout << "Redukty: " << diffs;
+  std::vector<Reduct> diffs = dt.reducts();
+  std::cout << "Redukty: " << diffs << std::endl;
 }
